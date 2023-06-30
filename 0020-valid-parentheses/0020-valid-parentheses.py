b3052ce07c45c0fdcaff045d89eee975d12c5eef
Semih702 <41264482+Semih702@users.noreply.github.com>
2023-06-30 14:25:50 +0300

    Time: 41 ms (87.34%), Space: 16.4 MB (51.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,9 +5,7 @@ class Solution:
         for i in s:
             if i in brackets:
                 stack.append(brackets[i])
-            elif not stack:
-                return False
-            elif i==stack[-1]:
+            elif stack and i==stack[-1]:
                 stack.pop()
             else:
                 return False
