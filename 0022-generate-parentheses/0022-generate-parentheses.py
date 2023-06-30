c1d29de38a78fb3baa0585c073a393552e0bc08b
Semih702 <41264482+Semih702@users.noreply.github.com>
2023-06-30 16:40:50 +0300

    Time: 47 ms (78.60%), Space: 16.7 MB (25.55%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,15 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         def generate(alist,n,op=0,cl=0):
-            combinations=[]
             if not n:
                 return [alist+")"*(op-cl)]
+            combinations=[]
             
-
             if not alist:
                 combinations.extend(generate("(",n-1,op+1,cl))
             else:
-                print(alist,n)
                 combinations.extend(generate(alist+"(",n-1,op+1,cl))
                 if op>cl:
                     combinations.extend(generate(alist+")",n,op,cl+1))
