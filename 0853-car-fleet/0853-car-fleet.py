66fa816375246969db4242e26b17575b39883f6f
Semih702 <41264482+Semih702@users.noreply.github.com>
2023-07-02 02:05:04 +0300

    Time: 1149 ms (7.42%), Space: 44.8 MB (5.12%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        time=[]
+        for i,j in enumerate(position):
+            time.append((target-j)/speed[i])
+        position,time=zip(*sorted(zip(position, time),reverse=True))
+        
+        fleet=1
+        current=time[0]
+        print(time)
+        for i in time[1:]:
+            if current and i>current:
+                fleet+=1
+                current=i
+        return fleet
+        
+        
+
+        
