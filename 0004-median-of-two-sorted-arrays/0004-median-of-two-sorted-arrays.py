bb86ff4488bef7fdfe5804d8c3dfa6c6f68b5bda
Semih702 <41264482+Semih702@users.noreply.github.com>
2023-08-02 23:34:25 +0300

    Time: 86 ms (97.46%), Space: 16.5 MB (95.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,8 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        fullist=sorted(nums1+nums2)
-        if len(fullist)%2==1:
-            return(fullist[int(len(fullist)/2-0.5)])
-        else:
-            
-            return((fullist[int(len(fullist)/2)]+fullist[int(len(fullist)/2-1)])/2)
+        size=len(nums1)+len(nums2)
+        low,high=nums1,nums2
+        if len(low)>len(high):
+            low,high=high,low
+        mid=size//2
+        l,r=0,len(low)-1
+        while True:
+            leftmid=(l+r)//2
+            rightmid=mid-leftmid-2
+
+            lowleft= low[leftmid] if leftmid>=0 else float("-inf")
+            lowright= low[leftmid+1] if leftmid+1<len(low) else float("inf")
+            highleft= high[rightmid] if rightmid>=0 else float("-inf")
+            highright= high[rightmid+1] if rightmid+1<len(high) else float("inf")
+
+            if lowleft<=highright and highleft<=lowright:
+                if size%2:
+                    return min(lowright,highright)
+                return (max(lowleft,highleft)+min(lowright,highright))/2
+            elif lowleft<=highright:
+                l=leftmid+1
+            else:
+                r=leftmid-1
+                
